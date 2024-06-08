0a707cd6b027de0c3a7ddb8ab830299439bad305
LucasHor4 <lucash.arcanjo267@gmail.com>
2024-06-08 13:13:14 -0300

    Realizando mudanças para implementar AnimatedContainer e GestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'dart:ui_web';
+
 import 'package:flutter/material.dart';
-import 'package:surfsp/praias.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const App());
+  runApp(Praias());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,22 +16,66 @@ class App extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
-            color: Color.fromARGB(255, 255, 55, 55),
+            color: const Color.fromARGB(255, 255, 55, 55),
             child: TextButton(
               onPressed: () {
-                //Navigator.pop(context);
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) =>
-                        MainApp(),
+                    builder: (context) => Praias(),
                   ),
                 );
               },
-              child: Text('Praias'),
+              child: const Text('Praias'),
             ),
             //fazer nessa pagina: barra de menu, seletor de idioma e botões para navegação de paginas.
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Praias extends StatefulWidget {
+  @override
+  State<Praias> createState() => PraiasState();
+}
+
+class PraiasState extends State<Praias> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        }); 
+      },// para alternar entre ligado e deligado.
+
+      child: Center(
+        child: AnimatedContainer(
+          margin: const EdgeInsets.only(
+            bottom: 20,
+          ),
+          width: selected ? 200 : 200,
+          height: selected ? 350 : 320,
+          color: selected
+              ? const Color.fromARGB(255, 28, 255, 8)
+              : const Color.fromARGB(255, 255, 0, 0),
+          duration: const Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+          //Container e configurações dele.
+
+          child: Container(
+            width: 40, height: 25,
+            margin:const EdgeInsets.only(
+              top: 30,
+              bottom: 25,
+            ),
+            child: Text(selected ? 'Foi' : '', textDirection: TextDirection.ltr,),
+          ), //Texto sobre as praias.
+
         ),
       ),
     );
