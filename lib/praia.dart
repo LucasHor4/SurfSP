c9199db18aef7ce6abea001e828814ffe5afc5a7
Mixhel Castro <castromichel694@gmail.com>
2024-09-11 10:24:47 -0300

    commit 11/9

---
+++ lib/praia.dart
@@ -0,0 +1,7 @@
+class praia{
+  late int idPraia;
+  final String nomePraia;
+  final String mostrarDesc;
+
+praia( this.idPraia, this.nomePraia, this.mostrarDesc);
+}
---
+++ lib/usuario.dart
@@ -0,0 +1,11 @@
+class usuario {
+  late int idUsuario;
+  final String nomeUsuario;
+
+  usuario(this.idUsuario, this.nomeUsuario);
+}   
+
+mudarIdioma(){
+  
+
+}
